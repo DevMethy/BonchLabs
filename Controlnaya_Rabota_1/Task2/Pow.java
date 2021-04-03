27aadce5461b81952411cf1317365293ab19749f
DevMethy <77387266+DevMethy@users.noreply.github.com>
2021-04-03 22:14:55 +0300

    Create Pow.java

---
+++ Controlnaya_Rabota_1/Task2/Pow.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(Math.pow(n, 2));
+        System.out.println(Math.pow(n, 3));
+    }
+}
