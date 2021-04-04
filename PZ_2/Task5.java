13de5b3164f7732f129ea8ba473b580813595e54
DevMethy <77387266+DevMethy@users.noreply.github.com>
2021-04-04 14:44:28 +0300

    Create Task5.java

---
+++ PZ_2/Task5.java
@@ -0,0 +1,30 @@
+package com.company;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        int i = 2015;
+        long l = (long) (i);
+        System.out.println(l);
+
+        int i2 = 2015;
+        float f = (float) (i2);
+        System.out.println(f);
+
+
+        long l1 = 214748364;
+        int i3 = (int) l1;
+        System.out.println(i3);
+
+
+        double d = 3.14;
+        int i4 = (int) d;
+        System.out.println(i4);
+
+
+
+
+    }
+
+}
