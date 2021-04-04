3230ff40fd6c9ad661d2a20eb6f53a9a0dd3694f
DevMethy <77387266+DevMethy@users.noreply.github.com>
2021-04-04 14:41:36 +0300

    Create Task3.java

---
+++ PZ_2/Task3.java
@@ -0,0 +1,17 @@
+package com.company;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        String s1 = "I have ";
+        String s2 = " apples ";
+        int num = 3;
+        String s = s1 + num + s2;
+
+        System.out.println(s);
+
+
+    }
+
+}
