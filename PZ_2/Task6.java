f5ce4f8b3bb5f7746640a3d13b2cfb8bff17e276
DevMethy <77387266+DevMethy@users.noreply.github.com>
2021-04-04 14:46:10 +0300

    Create Task6.java

---
+++ PZ_2/Task6.java
@@ -0,0 +1,40 @@
+package com.company;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        char ch = 'S';
+
+        // c использованием класса Character
+        String charToString = Character.toString(ch);
+        System.out.println(charToString);
+
+        // с использованием операции добавления класса String
+        String str = "" + ch;
+        System.out.println(str);
+
+        //с использованием массива
+        String fromChar = new String(new char[] { ch });
+        System.out.println(fromChar);
+
+        // с использованием метода valueOf класса String
+        String valueOfchar = String.valueOf(ch);
+        System.out.println(valueOfchar);
+
+
+        char ch1 = '9';
+
+        // c использованием метода getNumericValue
+        // класса Character
+        int i1 = Character.getNumericValue(ch1);
+        System.out.println(i1);
+
+        // c использованием метода digit класса Character
+        int i2 = Character.digit(ch1,10);
+        System.out.println(i2);
+
+
+    }
+
+}
