c29fd811306458ef5805b373d41459c9e1f56f43
DevMethy <77387266+DevMethy@users.noreply.github.com>
2021-04-04 14:43:12 +0300

    Create Task4.java

---
+++ PZ_2/Task4.java
@@ -0,0 +1,27 @@
+package com.company;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        int i = 35;
+        String str = Integer.toString(i);
+        System.out.println(str);
+
+        double  i2 = 32.4e10;
+        String str1 = Double.toString(i2);
+        System.out.println(str1);
+
+        long  i3 = 3422222;
+        String str2 = Long.toString(i3);
+        System.out.println(str2);
+
+        float  i4 = 3.46f;
+        String str3 = Float.toString(i4);
+        System.out.println(str3);
+
+
+
+    }
+
+}
