4dd1f9ab31fd036c81b0fbda63cbe5ed1970635b
DevMethy <77387266+DevMethy@users.noreply.github.com>
2021-04-04 14:39:56 +0300

    Create Task1.java

---
+++ PZ_2/Task1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        char symb1=1067;
+        char symb2 ='Ы';
+
+        System.out.println("symb1 contains "+ symb1);
+        System.out.println("symb2 contains "+ symb2);
+    }
+
+}
