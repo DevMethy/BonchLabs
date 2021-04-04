28b899448dfc0c97c125a1a28f1cc8d18931d6eb
DevMethy <77387266+DevMethy@users.noreply.github.com>
2021-04-04 14:40:46 +0300

    Create Task2.java

---
+++ PZ_2/Task2.java
@@ -0,0 +1,14 @@
+package com.company;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        char ch = 'J';
+        int intCh = (int) ch;
+
+        System.out.println("J corresponds with "+ intCh);
+
+    }
+
+}
